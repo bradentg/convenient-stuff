7cba4598e535edac6fe1e4b271a0cf4e35ce877a
Braden Goodwin <6876787+bradentg@users.noreply.github.com>
2024-10-11 20:42:08 -0400

    Fedora install script

---
+++ fedora40-installs.sh
@@ -0,0 +1,29 @@
+# RPM Fusion Configuration
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf config-manager --enable fedora-cisco-openh264
+sudo dnf update @core
+
+# Multimedia setup
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing
+sudo dnf group install Multimedia
+
+# VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf check-update
+sudo dnf install code # or code-insiders
+
+# LibreWolf
+curl -fsSL https://rpm.librewolf.net/librewolf-repo.repo | pkexec tee /etc/yum.repos.d/librewolf.repo
+sudo dnf install librewolf
+
+# Other apps
+sudo dnf install discord
+
+# Install Flatpak and enable remote
+sudo dnf install -y flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Install flatpak software
+flatpak install flathub com.spotify.Client
+
