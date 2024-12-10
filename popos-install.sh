222c941f5186f61f84e98b2fa50f5cfa12e0bc41
Braden Goodwin <6876787+bradentg@users.noreply.github.com>
2024-12-09 19:13:44 -0500

    Gaming section

--- popos-install.sh
+++ popos-install.sh
@@ -3,10 +3,14 @@ sudo apt update
 sudo apt install neovim
 sudo apt install gimp
 sudo apt-get install gparted
-sudo apt install gamemode
 sudo apt install nodejs
 sudo apt install npm
 sudo apt install timeshift
+
+# Gaming
+sudo apt install gamemode
+sudo apt install mangohud
+flatpak install flathub org.prismlauncher.PrismLauncher
 
 # Temurin 21 JDK
 sudo apt install -y wget apt-transport-https gpg
@@ -14,9 +18,6 @@ wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --
 echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
 sudo apt update
 sudo apt install temurin-21-jdk
-
-# Minecraft and Prism Launcher
-flatpak install flathub org.prismlauncher.PrismLauncher
 
 # Signal
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
