f544cdef578ba15ac3ca258da35f701f0fad8ab5
Braden Goodwin <6876787+bradentg@users.noreply.github.com>
2024-12-14 17:47:24 -0500

    reorg

--- popos-install.sh
+++ popos-install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+
 sudo apt update
+
+# Flatpak
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+###################
+### Development ###
+###################
+
 sudo apt install neovim
-sudo apt install gimp
-sudo apt-get install gparted
 sudo apt install nodejs
 sudo apt install npm
-sudo apt install timeshift
 
-# GitHub CLI
+# --- GitHub CLI
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
 	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
         && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
@@ -17,11 +24,24 @@ sudo apt install timeshift
 	&& sudo apt update \
 	&& sudo apt install gh -y
 
-# Gaming
+# --- Temurin 21 JDK
+sudo apt install -y wget apt-transport-https gpg
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
+sudo apt update
+sudo apt install temurin-21-jdk
+
+##############
+### Gaming ###
+##############
+
 sudo apt install gamemode
 sudo apt install mangohud
+flatpak install flathub com.heroicgameslauncher.hgl
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub org.prismlauncher.PrismLauncher
 
-# winehq-stable for Ubuntu 22.04 (jammy)
+# --- winehq-stable for Ubuntu 22.04 (jammy)
 sudo dpkg --add-architecture i386
 sudo mkdir -pm755 /etc/apt/keyrings
 wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
@@ -29,32 +49,35 @@ sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/
 sudo apt update
 sudo apt install --install-recommends winehq-stable
 
-# Temurin 21 JDK
-sudo apt install -y wget apt-transport-https gpg
-wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
-echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-sudo apt update
-sudo apt install temurin-21-jdk
+#############
+### Media ###
+#############
 
-# Signal
+sudo apt install picard
+sudo apt install gimp
+flatpak install flathub org.freac.freac
+
+# --- Spotify
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+############
+### Misc ###
+############
+
+sudo apt-get install gparted
+sudo apt install timeshift
+flatpak install flathub md.obsidian.Obsidian
+
+# --- Signal
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
 cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
   sudo tee /etc/apt/sources.list.d/signal-xenial.list
 sudo apt update && sudo apt install signal-desktop
 
-# Spotify
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
-
-# Flatpak
-sudo apt install flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub md.obsidian.Obsidian
-flatpak install flathub org.prismlauncher.PrismLauncher
-flatpak install flathub com.discordapp.Discord
-flatpak install flathub org.freac.freac
+###############################
 
 # zsh
 sudo apt install zsh
